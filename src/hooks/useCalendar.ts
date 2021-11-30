6982fb099ce1355e8bc28ccab2d74571dd69925e
hendrikniemann <h.neverman@web.de>
2021-11-30 18:10:53 +0100

    Change signature of setDate
    Set date can now be called with a date directly or with an update
    function that takes the old date and returns a new date, similar to
    react state update functions (in fact it is a React setState
    underneath). This makes it easier to implement going forward for a
    specified amount of days. This also works very well with the date-fns/fp
    package.

--- src/hooks/useCalendar.ts
+++ src/hooks/useCalendar.ts
@@ -3,16 +3,23 @@ import { useContext, createContext } from 'react';
 import { CalendarView } from '../types';
 
 export type CalendarContextType = {
-  /** The view that the calendar is currently displaying */
+  /** The currently focused date */
   date: Date;
+  /** The view that the calendar is currently displaying */
   view: CalendarView;
+  /** The start and end of the currently displayed view */
   viewPeriod: Interval;
+  /** Start and end of the visible part of each day in seconds since start of the day */
   viewTimes: { start: number; end: number };
+  /** Current setting for the amount of displayed custom days */
   customDays: number;
-  // Controls
-  setDate: (date: Date) => void;
+  /** Set the currently focused day. Provide a function to get the current date for easy updates! */
+  setDate: (date: Date | ((current: Date) => Date)) => void;
+  /** Set the calendar view */
   setView: (view: CalendarView) => void;
+  /** Move to next view period; this is done by moving the date for the length of a view peroid */
   goForward: () => void;
+  /** Move to previous view period; this is done by moving the date for the length of a peroid */
   goBackward: () => void;
 };
 
